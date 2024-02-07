112e35f82f1a95ecd6955cd47e60fa40334dbfdd
Alexander Kuptsov <kuptsov.dev@gmail.com>
2024-02-07 20:37:28 +0500

    created section Chef

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
+import { SubHeading } from "../../components";
+import { images } from "../../constans";
+
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam
+            eaque, at, quod minima labore, officia dolorem accusamus et magni
+            recusandae nihil! Sapiente dicta fugiat, dolorem amet nemo delectus
+            aliquid quo!
+          </p>
+        </div>
+        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio illum veritatis ratione, delectus veniam minima quos fugit vel est repellendus culpa? Accusamus minima illum nam ipsam suscipit nisi nostrum! Itaque?
+        Quos quam consequatur beatae vero eius in nisi, iusto eaque quasi saepe illum, voluptatum earum architecto enim doloribus. Consequuntur voluptatibus dignissimos dolorum nam voluptate necessitatibus cumque, corrupti tempore ipsam voluptates?
+        Accusantium laborum eius consectetur aliquam, quidem recusandae adipisci amet reiciendis vero tempore officiis deleniti illo at beatae nam explicabo provident quam velit doloremque dolore totam fuga dolorem. Recusandae, odio eligendi?
+        Quod enim reprehenderit quo impedit pariatur perspiciatis suscipit aliquid, tempore ipsum id, vitae illum. </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
